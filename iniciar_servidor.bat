0d8b1f652d8859f4e5a9b0d6b89d585c22785785
jotayepes <jotayepes@gmail.com>
2025-06-10 11:05:57 +0100

    optimize uvicorn CPU usage

--- iniciar_servidor.bat
+++ iniciar_servidor.bat
@@ -3,6 +3,6 @@ echo Activando entorno virtual...
 call env\Scripts\activate
 
 echo Iniciando servidor FastAPI en segundo plano...
-start /min cmd /k "uvicorn app.main:app --reload"
+start /min cmd /k "uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 1 --http h11"
 
 timeout /t 2 > nul
